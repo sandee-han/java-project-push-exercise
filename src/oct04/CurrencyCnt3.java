7051c75a7e9c09e0e758c81c06cf2ca752c0b399
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-04 14:46:16 +0900

    잔돈 나눗셈 단순 계산 코드(각 잔돈 별 화폐 갯수와 나머지 금액 + 누적 금액)

--- src/oct04/CurrencyCnt3.java
+++ src/oct04/CurrencyCnt3.java
@@ -14,13 +14,21 @@ public class CurrencyCnt3 {
         int refund = 25000;
 
         System.out.printf("5만원권 %d장, 나머지: %d\n", refund / curr50000, refund% curr50000);
+        refund%= curr50000;
         System.out.printf("1만원권 %d장 나머지: %d\n", refund / curr10000, refund % curr10000);
+        refund %= curr10000;
         System.out.printf("5천원권 %d장 나머지: %d\n", refund / curr5000, refund % curr5000);
+        refund %= curr5000;
         System.out.printf("천원권 %d장 나머지: %d\n", refund / curr1000, refund % curr1000);
+        refund %= curr1000;
         System.out.printf("500원 %d개 나머지: %d\n", refund / curr500, refund % curr500);
+        refund %= curr500;
         System.out.printf("100원 %d개 나머지: %d\n", refund / curr100, refund % curr100);
+        refund %= curr100;
         System.out.printf("50원 %d개 나머지: %d\n", refund / curr50, refund % curr50);
+        refund %= curr50;
         System.out.printf("10원 %d개 나머지: %d\n", refund / curr10, refund % curr10);
+
 
 
     }
