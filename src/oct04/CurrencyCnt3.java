7022a94a28aaaba20175ca8e610887343693d5c7
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-04 14:26:28 +0900

    잔돈 나눗셈 단순 계산 코드

---
+++ src/oct04/CurrencyCnt3.java
@@ -0,0 +1,25 @@
+package oct04;
+
+public class CurrencyCnt3 {
+    public static void main(String[] args) {
+        int curr50000 = 50000;
+        int curr10000 = 10000;
+        int curr5000 = 5000;
+        int curr1000 = 1000;
+        int curr500 = 500;
+        int curr100 = 100;
+        int curr50 = 50;
+        int curr10 = 10;
+
+        int refund = 25000;
+
+        System.out.printf("5만원권 %d장\n", refund / curr50000, refund%= curr50000);
+        System.out.printf("1만원권 %d장\n", refund / curr10000, refund %= curr10000);
+        System.out.printf("5천원권 %d장\n", refund / curr5000, refund %= curr5000);
+        System.out.printf("천원권 %d장\n", refund / curr1000, refund %= curr1000);
+        System.out.printf("500원 %d개\n", refund / curr500, refund %= curr500);
+        System.out.printf("100원 %d개\n", refund / curr100, refund %= curr100);
+        System.out.printf("50원 %d개\n", refund / curr50, refund %= curr50);
+        System.out.printf("10원 %d개\n", refund / curr10);
+    }
+}
