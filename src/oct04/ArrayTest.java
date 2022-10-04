e343023e2afdbf593b9b110be3a52b7d6ea69542
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-04 13:48:47 +0900

    ArrayTest 에 배열 입력으로 수정 CurrenyCnt로 금액 확인 클래스

--- src/oct04/ArrayTest.java
+++ src/oct04/ArrayTest.java
@@ -1,69 +1,15 @@
 package oct04;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.printf("금액을 넣으세요: ");
-        int money = scan.nextInt();
-        int change = 0;
-        int count = 0;
-        if (money > 50000) {
-            count = money / 50000;
-            money %= 50000;
-            System.out.println("50000원 " + count + "개");
-        } else {
-            System.out.println("50000원 0개");
+        int[] array = new int[10];
+        for(int i = 0; i <= 9; i++){
+            array[i] = i;
         }
-        if (money > 10000) {
-            count = money / 10000;
-            money %= 10000;
-            System.out.println("10000원 " + count + "개");
-        }else {
-            System.out.println("10000원 0개");
-        }
-        if (money > 5000) {
-            count = money / 5000;
-            money %= 5000;
-            System.out.println("5000원 " + count + "개");
-        }else {
-            System.out.println("5000원 0개");
-        }
-        if (money > 1000) {
-            count = money / 1000;
-            money %= 1000;
-            System.out.println("1000원 " + count + "개");
-        }else {
-            System.out.println("1000원 0개");
-        }
-        if (money > 500) {
-            count = money / 500;
-            money %= 500;
-            System.out.println("500원 " + count + "개");
-        }else {
-            System.out.println("500원 0개");
-        }
-        if (money > 100) {
-            count = money / 100;
-            money %= 100;
-            System.out.println("100원 " + count + "개");
-        }else {
-            System.out.println("100원 0개");
-        }
-        if (money > 50) {
-            count = money / 50;
-            money %= 50;
-            System.out.println("50원 " + count + "개");
-        }else {
-            System.out.println("50원 0개");
-        }
-        if (money > 10){
-            count = money / 10;
-            money %= 10;
-            System.out.println("10원" + count + "개");
-        }else {
-            System.out.println("10원 0개");
-        }
+        System.out.println(Arrays.toString(array));
     }
+
 }
