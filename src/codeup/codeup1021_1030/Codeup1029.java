5f3418c688e957879222bf5f5621319f571531fe
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-06 18:02:42 +0900

    Codeup 1021-1030 code (1029 update)

--- src/codeup/codeup1021_1030/Codeup1029.java
+++ src/codeup/codeup1021_1030/Codeup1029.java
@@ -1,2 +1,14 @@
-package codeup.codeup1021_1030;public class Codeup1029 {
+package codeup.codeup1021_1030;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Codeup1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double input = sc.nextDouble();
+        DecimalFormat form = new DecimalFormat("#.00000000000");
+        System.out.println(form.format(input));
+    }
 }
