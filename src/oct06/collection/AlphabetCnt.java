be9d4e2ccd287466f8ba79f733869b169f7b39fd
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-06 13:22:27 +0900

    String 한 글자씩 출력, 알파벳인지 확인하는 function

---
+++ src/oct06/collection/AlphabetCnt.java
@@ -0,0 +1,28 @@
+package oct06.collection;
+
+public class AlphabetCnt {
+
+    //대문자 65 ~ 90, 소문자 97~122 if((c >= 65 and c< 90) or ())
+    // 알파벳인지 확인하는 function
+    public static boolean isAlphabet(char c) {
+        if((int)c >= 97 && (int)c < 122){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab53c12cd:d'e2fghij21kkkkkl12lm";
+
+        // String을 한글자씩 출력할 수 있어야한다.
+        for(int i = 0; i < s1.length(); i++){
+            if(isAlphabet(s1.charAt(i))) {
+                System.out.print(s1.charAt(i) + " ");
+            }
+        }
+
+
+    }
+
+}
