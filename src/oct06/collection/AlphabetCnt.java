5ed958b497c8238e9a59ebb6622f14540bd0a3a6
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-06 13:30:17 +0900

    test

--- src/oct06/collection/AlphabetCnt.java
+++ src/oct06/collection/AlphabetCnt.java
@@ -4,7 +4,7 @@ public class AlphabetCnt {
 
     //대문자 65 ~ 90, 소문자 97~122 if((c >= 65 and c< 90) or ())
     // 알파벳인지 확인하는 function
-    public static boolean isAlphabet(char c) {
+    public boolean isAlphabet(char c) {
         if((int)c >= 97 && (int)c < 122){
             return true;
         } else {
@@ -13,11 +13,12 @@ public class AlphabetCnt {
     }
 
     public static void main(String[] args) {
+        AlphabetCnt alphabetCnt = new AlphabetCnt();
         String s1 = "ab53c12cd:d'e2fghij21kkkkkl12lm";
 
         // String을 한글자씩 출력할 수 있어야한다.
         for(int i = 0; i < s1.length(); i++){
-            if(isAlphabet(s1.charAt(i))) {
+            if(alphabetCnt.isAlphabet(s1.charAt(i))) {
                 System.out.print(s1.charAt(i) + " ");
             }
         }
