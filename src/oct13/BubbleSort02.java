6d90599605e4f95f1d4d1c25cd139fc60dc451a7
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-13 10:10:22 +0900

    이중 for문 Sort

--- src/oct13/BubbleSort02.java
+++ src/oct13/BubbleSort02.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 public class BubbleSort02 {
     public int[] Sort(int[] arr) {
         int temp;
-        for (int i = 1; i < arr.length; i++) {
-            for(int j = 0; j < arr.length; j++){
-                if (arr[j] > arr[i]) {
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+        for (int i = 0; i < arr.length-1; i++) {
+            for(int j = i + 1; j < arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
