75f751fbaae7df65777c160c6284bac22e752b43
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-13 09:47:37 +0900

    Sort 완성

--- src/oct13/BubbleSort01.java
+++ src/oct13/BubbleSort01.java
@@ -1,2 +1,28 @@
-package oct13;public class BubbleSort01 {
+package oct13;
+
+import java.util.Arrays;
+
+public class BubbleSort01 {
+
+    public int[] Sort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            if(array[i] > array[i+1]){
+                temp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort01 bubbleSort01 = new BubbleSort01();
+
+        int[] arr = new int[]{7, 2, 3, 9, 28, 11};
+
+        System.out.println(Arrays.toString(bubbleSort01.Sort(arr)));
+
+    }
 }
--- src/oct13/BubbleSort02.java
+++ src/oct13/BubbleSort02.java
@@ -3,27 +3,25 @@ package oct13;
 
 import java.util.Arrays;
 
-public class SelectionSort01 {
-
-
-    public int[] Sort(int[] arr){
+public class BubbleSort02 {
+    public int[] Sort(int[] arr) {
         int temp;
-        for(int i = 1 ; i < arr.length ; i ++) {
-            if(arr[0] > arr[i]) {
-                temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
+        for (int i = 1; i < arr.length; i++) {
+            for(int j = 0; j < arr.length; j++){
+                if (arr[j] > arr[i]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
             }
         }
-
-
         return arr;
     }
 
     public static void main(String[] args) {
-        SelectionSort01 selectionSort = new SelectionSort01();
+        BubbleSort02 bubbleSort02 = new BubbleSort02();
         int[] arr = new int[]{7, 2, 3, 9, 28, 11};
 
-        System.out.println(Arrays.toString( selectionSort.Sort(arr) ));
+        System.out.println(Arrays.toString(bubbleSort02.Sort(arr)));
     }
 }
