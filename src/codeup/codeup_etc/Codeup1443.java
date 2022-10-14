43a3d9e6611436fa725175625cf650f13297701c
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-14 10:38:03 +0900

    InsertionSort: Codeup1443

---
+++ src/codeup/codeup_etc/Codeup1443.java
@@ -0,0 +1,30 @@
+package codeup.codeup_etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Codeup1443 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int count = Integer.parseInt(input);
+        int[] arr = new int[count];
+        for(int i = 0; i < count; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 1; i < arr.length; i++){
+            for(int j = i; j > 0; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
--- src/oct14/InsertionSort01.java
+++ src/oct14/InsertionSort01.java
@@ -1,9 +1,12 @@
 package oct14;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class InsertionSort01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int[] arr = {8, 5, 6, 2, 4};
         Sort sort = new Sort();
         System.out.println(Arrays.toString(sort.Swap(arr)));
