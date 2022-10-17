6650041fc2b6eb86213d2383612bd59f36f061fd
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-17 09:09:50 +0900

    RightTriangle with Star

--- src/oct17/RightTriangle.java
+++ src/oct17/RightTriangle.java
@@ -9,12 +9,6 @@ public class RightTriangle {
         * * * *
         * * * * *
          */
-        for (int i = 1; i < 5; i++){
-            for (int j = 0; j < i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println("");
-        }
         System.out.println("*");
         System.out.println("* *");
         System.out.println("* * *");
