f563391b7fde7db0871424d7917db2d6248f7e5d
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-18 09:22:49 +0900

    Square, Rectangle Star

---
+++ src/oct18/SquareAlgorithmMain.java
@@ -0,0 +1,11 @@
+package oct18;
+
+import java.io.IOException;
+
+public class SquareAlgorithmMain {
+    public static void main(String[] args) throws IOException {
+        SquareStar squareStar = new SquareStar();
+        squareStar.Square(squareStar.getSize());
+        squareStar.Rectangle(squareStar.getSize(), squareStar.getSize());
+    }
+}
--- src/oct18/SquareStar.java
+++ src/oct18/SquareStar.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SquareStar {
-    public static void main(String[] args) throws IOException {
+    public int getSize() throws IOException {
+        System.out.print("size: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        int size = Integer.parseInt(input);
+        return Integer.parseInt(input);
+    }
 
+    public void Square(int size) {
         for (int i = 1; i <= size; i++) {
             for(int j = 1; j <= size; j++) {
                 System.out.print("*");
@@ -17,4 +20,14 @@ public class SquareStar {
             System.out.println("");
         }
     }
+
+    public  void Rectangle(int x_size, int y_size) {
+        for (int i = 1; i <= y_size; i++) {
+            for(int j = 1; j <= x_size; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
 }
